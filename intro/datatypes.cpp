9866a17a8c2c8990dbf1faede6ea0a4f261e629c
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2023-01-03 21:49:56 +0300

    added datatypes

---
+++ intro/datatypes.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+     /*
+    1. Integer
+    The keyword int can represent integer data types. The range of integers is -2147483648 to 2147483647, and they take up 4 bytes of memory.
+
+    For example,
+     */
+    int data = 1526;
+    
+    /*
+    2. Character
+    The keyword char represent characters. It is 1 byte in size. Single quotes ' ' are used to enclose characters in C++.
+
+    For example,
+    */
+    char ch = 's';
+
+    /*
+    3. Boolean
+    The boolean data type's keyword is bool. True or false are the two possible values for the boolean data type. Boolean values are generally used in conditional statements and loops.
+
+    For example,
+    */
+    bool is_true = true; // or false
+
+    /*
+    4. Floating Point
+    float is the keyword used to hold floating-point numbers (decimals and exponentials). The float variable has a size of 4 bytes.
+
+    For example,
+    */
+    float val = 15.26;
+
+    /*
+
+    5. Double Floating Point
+    double is the keyword used to hold floating-point numbers (decimals and exponentials) with double precision. The double variable has a size of 8 bytes.
+
+    For example,
+    */
+
+    double val = 2019.1526;
+
+    /*
+    6. Void or Valueless
+    The term void refers to something that has no worth. The void data type represents a valueless entity. Variables of the void type cannot be declared. It is only used for functions, not returning any data.
+    */
+
+    /*
+    7. Wide Character
+    The wide-character wchar_t data type is similar to the char data type, but its size is 2 or 4 bytes rather than 1 byte. It's used to represent characters that take up more memory than a single char to represent.
+
+    For example,
+    */
+    wchar_t w = L'C';
+
+   
+}
